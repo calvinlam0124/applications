83607dcf06bbe9f4091cfffcd4b46a4a5e529862
Calvin Lam <calvinlam117@gmail.com>
2020-09-02 23:34:37 +0800

    test: add jwt-secret and app-key existent test

---
+++ docker-for-laravel/laravel-test/Unit/JwtTest.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class JwtTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testSecretExistTest(): void
+    {
+        $file = parse_ini_file('.env', true, INI_SCANNER_RAW);
+        $this->assertNotNull($file['JWT_SECRET']);
+    }
+}
