55b444d09c94b247fe7cbefb8866ae48dc2311f2
Calvin Lam <calvinlam117@gmail.com>
2021-01-20 00:42:46 +0800

    feat: set slimphp route name

--- php-slimphp-skeleton/code/app/routes.php
+++ php-slimphp-skeleton/code/app/routes.php
@@ -7,15 +7,16 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Routing\RouteContext;
 
 return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('Hello world!');
         return $response;
-    });
+    })->setName('home');
 
     $app->group('/users', function (Group $group) {
-        $group->get('', ListUsersAction::class);
+        $group->get('', ListUsersAction::class)->setName('users.list');
         $group->get('/{id}', ViewUserAction::class);
     });
 };
